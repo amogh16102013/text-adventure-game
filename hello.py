9e08abaaa6d03a67cc58c47c3c620867124aab6d
amogh16102013 <amogh.nalli@gmail.com>
2026-06-20 11:44:54 -0400

    bill calculator

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
-amt = float(input("how much did you pay \n"))
-print("total:")
-print(amt + 100)
+bill = float(input("how much is your bill?\n"))
+tip = int(input("how much do you want to tip?\n"))
+total_tip = bill * tip/100
+total = total_tip + bill
+print("here is your tip: $", total_tip)
+print("here is your bill: $", total)
