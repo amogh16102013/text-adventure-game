3ef55f9a8dab96529e86a8fb818a3addef77de6c
amogh16102013 <amogh.nalli@gmail.com>
2026-06-27 10:09:55 -0400

    if and else

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@
 # print("here is your bill: $", total)
 
 # for tip for lots of people
-people = int(input("how many people are there?"))
+people = int(input("how many people are there?\n"))
 bill = float(input("how much is your bill?\n"))
 tip = int(input("how much do you want to tip?\n"))
 total_tip = bill * tip/100
@@ -17,3 +17,7 @@ print("here is the total tip: $", round(total_tip,2))
 print("here is the total bill: $", round(total,2))
 print("here is how much each person will tip: $", round(shared_tip,2))
 print("here is how much each person will pay: $", shared_total)
+
+if 8 > 9:
+    print("8 is greater than 9")
+if 8>9:
